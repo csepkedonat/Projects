b36cf964f73ad426ef16b57e8d76e57b9320c820
csepkedonat <csepkedonat@gmail.com>
2025-05-06 01:49:12 +0200

    Add files via upload
    Ez a projekt egy objektumorientált konzolos alkalmazás C# nyelven, amely egy kávéfőző gép alapvető működését szimulálja. A kód lehetővé teszi a felhasználó számára, hogy interakcióba lépjen a virtuális kávéfőzővel: feltöltheti kávéval és vízzel, lefőzheti a kávét, megkóstolhatja az elkészült italt, kiürítheti az edényt, és lekérdezheti a gép állapotát.
    
    A projekt a C# programozási nyelv alapvető koncepcióit, mint az osztályok, objektumok, tulajdonságok és metódusok alkalmazását demonstrálja egy gyakorlatias példán keresztül. A kód struktúrája a valós világbeli kávéfőző működését tükrözi, logikus állapotkezeléssel és felhasználóbarát visszajelzésekkel a konzolon. Ez a projekt bemutatja a C# nyelvi készségeimet, az objektumorientált tervezési elveket és a konzolos alkalmazások fejlesztésének képességét. A forráskód a GitHub repository-ban megtekinthető.

---
+++ kavefozo/kavefozo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kavefozo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kavefozo kave = new Kavefozo();
+
+            kave.foz();
+
+            kave.feltolt("viz", 10);
+            kave.feltolt("kave", 6);
+
+            kave.foz();
+
+            kave.ranez();
+
+            kave.kiont();
+
+            Console.WriteLine(kave.Edeny.Minoseg);
+
+            kave.feltolt("kave", 3);
+
+            kave.kiurit("kave");
+
+            kave.feltolt("kave", 6);
+
+            kave.foz();
+
+            kave.ranez();
+
+            kave.kostol();
+
+            //Console.ReadLine();
+
+        }
+    }
+}
